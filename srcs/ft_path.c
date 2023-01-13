c15ca80986ab66b2873023202aa5219c06dbd2e4
Louis Bouchonville <lbouchon@fu-r6-p4.s19.be>
2023-01-13 15:18:55 +0100

    End

--- srcs/ft_path.c
+++ srcs/ft_path.c
@@ -6,7 +6,7 @@
 /*   By: lbouchon <lbouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 14:14:49 by lbouchon          #+#    #+#             */
-/*   Updated: 2023/01/12 17:52:29 by lbouchon         ###   ########.fr       */
+/*   Updated: 2023/01/13 15:17:05 by lbouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,21 @@ char	**duplicate_map(t_data *data)
 
 void	ft_path_help(t_path *path, char **map, int y, int x)
 {
+	if (map[y][x + 1] != '1')
+	{
+		if (map[y][x + 1] == 'C')
+			path->coins++;
+		if (map[y][x + 1] == 'E')
+			path->exit++;
+		map[y][x + 1] = '1';
+		ft_path(path, map, y, x + 1);
+	}
 	if (map[y][x - 1] != '1')
 	{
-		(map[y][x - 1] == 'C' && path->coins++) ||
-			(map[y][x - 1] == 'E' && path->exit++);
+		if (map[y][x - 1] == 'C')
+			path->coins++;
+		if (map[y][x - 1] == 'E')
+			path->exit++;
 		map[y][x - 1] = '1';
 		ft_path(path, map, y, x - 1);
 	}
@@ -54,24 +65,21 @@ void	ft_path(t_path *path, char **map, int y, int x)
 {
 	if (map[y + 1][x] != '1')
 	{
-		(map[y + 1][x] == 'C' && path->coins++) ||
-			(map[y + 1][x] == 'E' && path->exit++);
+		if (map[y + 1][x] == 'C')
+			path->coins++;
+		if (map[y + 1][x] == 'E')
+			path->exit++;
 		map[y + 1][x] = '1';
 		ft_path(path, map, y + 1, x);
 	}
 	if (map[y - 1][x] != '1')
 	{
-		(map[y - 1][x] == 'C' && path->coins++) ||
-			(map[y - 1][x] == 'E' && path->exit++);
+		if (map[y - 1][x] == 'C')
+			path->coins++;
+		if (map[y - 1][x] == 'E')
+			path->exit++;
 		map[y - 1][x] = '1';
 		ft_path(path, map, y - 1, x);
-	}
-	if (map[y][x + 1] != '1')
-	{
-		(map[y][x + 1] == 'C' && path->coins++) ||
-			(map[y][x + 1] == 'E' && path->exit++);
-		map[y][x + 1] = '1';
-		ft_path(path, map, y, x + 1);
 	}
 	ft_path_help(path, map, y, x);
 }
@@ -82,11 +90,9 @@ int	check_paths(t_data *data, int pos_y, int pos_x, int collected)
 	t_path	path;
 
 	dup = duplicate_map(data);
-	path.coins = 1;
-	path.exit = 1;
+	path.coins = 0;
+	path.exit = 0;
 	ft_path(&path, dup, pos_y, pos_x);
-	path.coins--;
-	path.exit--;
 	pos_y = 0;
 	while (dup[pos_y])
 		free(dup[pos_y++]);
